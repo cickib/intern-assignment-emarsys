7951f43490e28084511a037f25378f3a3757a960
cickib <bellakocsis06@gmail.com>
2017-03-20 23:23:36 +0100

    HolidayTest - oneDestinationAddedToHoliday_routeSizeIsOne

--- src/test/java/holiday_planner/HolidayTest.java
+++ src/test/java/holiday_planner/HolidayTest.java
@@ -3,18 +3,30 @@ package holiday_planner;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class HolidayTest {
 
     private Holiday holiday;
 
+    private Destination destinationX;
+
     @Before
     public void setUp() throws Exception {
         holiday = new Holiday();
+        destinationX = new Destination("x");
     }
 
     @Test(expected = NullPointerException.class)
     public void planRouteWithoutDestinations_throwsNullPointerException() {
         holiday.planRoute();
+    }
+
+    @Test
+    public void oneDestinationAddedToHoliday_routeSizeIsOne() {
+        holiday.addDestination(destinationX);
+        holiday.planRoute();
+        assertEquals(1, holiday.getRoute().size());
     }
 
 }
