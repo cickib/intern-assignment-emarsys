e372b33c3be00b14a3e533392fcfc6157dedd5bf
cickib <bellakocsis06@gmail.com>
2017-03-20 22:47:03 +0100

    Destination fields and constructor created

---
+++ src/main/java/holiday_planner/Destination.java
@@ -0,0 +1,14 @@
+package holiday_planner;
+
+
+public class Destination {
+    
+    private String destinationName;
+
+    private Destination nextDestination;
+
+    public Destination(String destinationName) {
+        this.destinationName = destinationName;
+    }
+
+}
