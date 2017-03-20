2271e6a3da200c12d9316b88942d53e4b0e817ca
cickib <bellakocsis06@gmail.com>
2017-03-20 23:01:04 +0100

    DestinationTest initial values created

--- src/test/java/holiday_planner/DestinationTest.java
+++ src/test/java/holiday_planner/DestinationTest.java
@@ -1,5 +1,18 @@
 package holiday_planner;
 
+import org.junit.Before;
+
 public class DestinationTest {
+
+    private Destination destinationX;
+    private Destination destinationY;
+    private Destination destinationZ;
+
+    @Before
+    public void setUp() throws Exception {
+        destinationX = new Destination("x");
+        destinationY = new Destination("y");
+        destinationZ = new Destination("z");
+    }
 
 }
