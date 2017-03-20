f5f1ccb38573b56bfb32884b31611632c4837955
cickib <bellakocsis06@gmail.com>
2017-03-20 23:09:33 +0100

    Holiday fields and constructor created

---
+++ src/main/java/holiday_planner/Holiday.java
@@ -0,0 +1,17 @@
+package holiday_planner;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Holiday {
+
+    private List<Destination> destinations;
+    private List<Destination> route;
+
+    public Holiday() {
+        destinations = new ArrayList<>();
+        route = new ArrayList<>();
+    }
+    
+}
